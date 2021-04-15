c669b10677a354b2b39347f14471d9665db175d0
Henry Schreiner <henry.fredrick.schreiner@cern.ch>
2021-04-15 18:22:36 -0400

    docs: remove recommonmark (#14)

--- {{cookiecutter.project_name}}/docs/conf.py
+++ {{cookiecutter.project_name}}/docs/conf.py
@@ -22,13 +22,15 @@ author = "{{ cookiecutter.full_name }}"
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    "myst_parser",
     "sphinx.ext.autodoc",
     "sphinx.ext.mathjax",
     "sphinx.ext.napoleon",
+    "sphinx_copybutton",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ["_templates"]
+templates_path = []
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -41,7 +43,19 @@ exclude_patterns = ["_build", "**.ipynb_checkpoints", "Thumbs.db", ".DS_Store", 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinx_rtd_theme"
+html_theme = "sphinx_book_theme"
+
+html_title = f"Hist {version}"
+
+html_baseurl = "https://{{ cookiecutter.project_name.replace("-", "_") }}.readthedocs.io/en/latest/"
+
+html_theme_options = {
+    "home_page_in_toc": True,
+    "repository_url": "{{ cookiecutter.url }}",
+    "use_repository_button": True,
+    "use_issues_button": True,
+    "use_edit_page_button": True,
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
