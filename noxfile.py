9eaeb192e4bfe7c6074f45e7af143f59e7492ace
Henry Schreiner <henryschreineriii@gmail.com>
2021-06-10 19:07:05 -0400

    ci: use nox for testing (+fix)

--- noxfile.py
+++ noxfile.py
@@ -6,16 +6,10 @@ DIR = Path(__file__).parent.resolve()
 BACKENDS = "setuptools", "pybind11", "flit", "poetry"
 
 
-@nox.session()
-@nox.parametrize("backend", BACKENDS, ids=BACKENDS)
-def lint(session, backend):
-    session.install("cookiecutter", "pre-commit")
-
+def make_cookie(session: nox.Session, backend: str) -> str:
     tmp_dir = session.create_tmp()
     session.cd(tmp_dir)
-    session.run(
-        "git", "clone", "--no-checkout", "--local", str(DIR), "tmp_git", external=True
-    )
+
     session.run(
         "cookiecutter",
         "--no-input",
@@ -23,8 +17,20 @@ def lint(session, backend):
         "--config-file",
         str(DIR / f"tests/{backend}.yml"),
     )
-
     session.cd(f"cookie-{backend}")
+    return tmp_dir
+
+
+@nox.session()
+@nox.parametrize("backend", BACKENDS, ids=BACKENDS)
+def lint(session: nox.Session, backend: str) -> None:
+    session.install("cookiecutter", "pre-commit")
+
+    make_cookie(session, backend)
+
+    session.run(
+        "git", "clone", "--no-checkout", "--local", str(DIR), "../tmp_git", external=True
+    )
     shutil.move("../tmp_git/.git", ".git")
     session.run("git", "add", ".", external=True)
 
@@ -35,3 +41,24 @@ def lint(session, backend):
         "--hook-stage=manual",
         "--show-diff-on-failure",
     )
+
+
+@nox.session()
+@nox.parametrize("backend", BACKENDS, ids=BACKENDS)
+def tests(session, backend):
+    session.install("cookiecutter")
+
+    make_cookie(session, backend)
+
+    session.install(".[test]", env={"SETUPTOOLS_SCM_PRETEND_VERSION": "0.1.0"})
+    session.run("python", "-m", "pytest", "-ra")
+
+
+@nox.session()
+def tests_poetry(session):
+    session.install("cookiecutter", "poetry")
+
+    make_cookie(session, "poetry")
+
+    session.run("poetry", "install")
+    session.run("poetry", "run", "pytest")
