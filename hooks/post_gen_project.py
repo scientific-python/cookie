89093992a241920a2729d83bbebe811e86420444
Henry Schreiner <henryschreineriii@gmail.com>
2021-06-11 18:34:34 -0400

    fix: hack the structure to remove src for trampolim

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -8,8 +8,10 @@ other_project_types = project_types - {project_type}
 project_underscore_name = project_name.replace("-", "_")
 project_dash_name = project_name.replace("_", "-")
 
+src = Path("src")
+
 if project_name != project_underscore_name:
-    Path(f"src/{project_name}").rename(f"src/{project_underscore_name}")
+    (src / project_name).rename(src / project_underscore_name)
 
 files = [p for p in Path(".").rglob("*") if p.is_file() and "-" in p.stem]
 
@@ -21,3 +23,10 @@ for f in files:
         f.replace(f.with_name(f"{base}{f.suffix}"))
     elif currents & other_project_types:
         f.unlink()
+
+# Hackaround lack of "src/" support in trampolim - remove when proper support
+# is added (and maybe after editable support is added?)
+# Also remove if clause in .pre-commit-config.yaml
+if project_type == "trampolim":
+    for f in Path("src").iterdir():
+        f.rename(f.stem)
