f83281f125eccdee0289005e7819a27785e364a9
Henry Schreiner <henryschreineriii@gmail.com>
2021-09-29 08:34:08 -0400

    chore: add clang formatting to pybind11 example

--- {{cookiecutter.project_name}}/src/main-pybind11.cpp
+++ {{cookiecutter.project_name}}/src/main-pybind11.cpp
@@ -1,30 +1,28 @@
 #include <pybind11/pybind11.h>
 
-int add(int i, int j) {
-    return i + j;
-}
+int add(int i, int j) { return i + j; }
 
 namespace py = pybind11;
 
 PYBIND11_MODULE(_core, m) {
-    m.doc() = R"pbdoc(
-        Pybind11 example plugin
-        -----------------------
-        .. currentmodule:: python_example
-        .. autosummary::
-           :toctree: _generate
-           add
-           subtract
-    )pbdoc";
+  m.doc() = R"pbdoc(
+      Pybind11 example plugin
+      -----------------------
+      .. currentmodule:: python_example
+      .. autosummary::
+         :toctree: _generate
+         add
+         subtract
+  )pbdoc";
 
-    m.def("add", &add, R"pbdoc(
-        Add two numbers
-        Some other explanation about the add function.
-    )pbdoc");
+  m.def("add", &add, R"pbdoc(
+      Add two numbers
+      Some other explanation about the add function.
+  )pbdoc");
 
-    m.def("subtract", [](int i, int j) { return i - j; }, R"pbdoc(
-        Subtract two numbers
-        Some other explanation about the subtract function.
-    )pbdoc");
-
+  m.def(
+      "subtract", [](int i, int j) { return i - j; }, R"pbdoc(
+      Subtract two numbers
+      Some other explanation about the subtract function.
+  )pbdoc");
 }
