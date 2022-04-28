d5b7ad7b5e4849872aadc609d2aaf8cca7c11f65
Henry Schreiner <henryschreineriii@gmail.com>
2022-04-28 09:52:26 -0400

    feat: skbuild backend

---
+++ {{cookiecutter.project_name}}/setup-skbuild.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# Copyright (c) {{ cookiecutter.year }}, {{ cookiecutter.full_name }}
+#
+# Distributed under the 3-clause BSD license, see accompanying file LICENSE
+# or {{ cookiecutter.url }} for details.
+
+from __future__ import annotations
+
+from setuptools import find_packages
+from skbuild import setup
+
+setup(
+    name="{{ cookiecutter.project_name.replace("-","_") }}",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    include_package_data=True,
+    cmake_install_dir="src/{{ cookiecutter.project_name.replace("-", "_") }}",
+)
