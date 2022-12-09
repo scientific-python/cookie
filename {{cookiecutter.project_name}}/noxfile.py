20d5ce4a3a7cd710123634087b9b6e9dd68b8290
Henry Schreiner <henryschreineriii@gmail.com>
2022-12-09 00:09:59 -0500

    fix: pylint misconfiguration
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>

--- {{cookiecutter.project_name}}/noxfile.py
+++ {{cookiecutter.project_name}}/noxfile.py
@@ -33,7 +33,7 @@ def pylint(session: nox.Session) -> None:
     # This needs to be installed into the package environment, and is slower
     # than a pre-commit check
     session.install(".", "pylint")
-    session.run("pylint", "src", *session.posargs)
+    session.run("pylint", "{{ cookiecutter.project_name.replace("-", "_") }}", *session.posargs)
 
 
 @nox.session
