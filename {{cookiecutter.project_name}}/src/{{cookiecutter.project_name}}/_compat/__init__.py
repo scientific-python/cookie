f1ee9a42f4539c257622a3edf3f7ce53a06c49c0
Henry Schreiner <henry.fredrick.schreiner@cern.ch>
2023-02-10 10:47:05 -0500

    feat: move to using Ruff (#119)
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>

---
+++ {{cookiecutter.project_name}}/src/{{cookiecutter.project_name}}/_compat/__init__.py
@@ -0,0 +1,8 @@
+"""
+Copyright (c) {{ cookiecutter.year }} {{ cookiecutter.full_name }}. All rights reserved.
+
+{{ cookiecutter.project_name }}: {{ cookiecutter.project_short_description }}
+"""
+
+
+from __future__ import annotations
