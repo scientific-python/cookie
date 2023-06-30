5a57356af7eefec3b3dba7311d51282d40d826a7
Henry Schreiner <henry.fredrick.schreiner@cern.ch>
2023-06-30 18:10:31 -0400

    fix: copier and pydantic 2 are not compatible (#198)

--- noxfile.py
+++ noxfile.py
@@ -258,7 +258,10 @@ def nox_session(session, backend):
 
 @nox.session()
 def compare_copier(session):
-    session.install("cookiecutter", "copier", "copier-templates-extensions")
+    # Pydantic 2.0 breaks copier <= 8.0.0
+    session.install(
+        "cookiecutter", "copier", "copier-templates-extensions", "pydantic<2"
+    )
 
     tmp_dir = session.create_tmp()
     session.cd(tmp_dir)
