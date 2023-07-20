97da6bcc894f26f4191e418c9f83dd07bbc8db77
Henry Schreiner <henry.fredrick.schreiner@cern.ch>
2023-07-20 10:42:53 -0400

    fix: drop _compat pre-filled module (#232)

--- {{cookiecutter.project_name}}/src/{{cookiecutter.__project_slug}}/_compat/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Copyright (c) {{ cookiecutter.__year }} {{ cookiecutter.full_name }}. All rights reserved.
-
-{{ cookiecutter.project_name }}: {{ cookiecutter.project_short_description }}
-"""
-
-
-from __future__ import annotations
--- {{cookiecutter.project_name}}/tests/test_package.py
+++ {{cookiecutter.project_name}}/tests/test_package.py
@@ -1,14 +1,7 @@
 from __future__ import annotations
 
 import {{ cookiecutter.__project_slug }} as m
-import {{ cookiecutter.__project_slug }}._compat.typing as typing_backports
 
 
 def test_version():
     assert m.__version__
-
-
-def test_has_typing():
-    assert hasattr(typing_backports, "TypeAlias")
-    assert hasattr(typing_backports, "Self")
-    assert hasattr(typing_backports, "assert_never")
