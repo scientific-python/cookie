d7c91e56fc6dc833754725741761c4a3950e9714
Henry Schreiner <henryschreineriii@gmail.com>
2024-04-22 02:30:45 -0400

    tests: use most recent repo-review in testing
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>

--- tests/test_pyproject.py
+++ tests/test_pyproject.py
@@ -1,95 +1,94 @@
-from sp_repo_review._compat import tomllib
-from sp_repo_review.checks import pyproject
+from repo_review.testing import compute_check, toml_loads
 
 
 def test_PP002_okay():
-    toml = tomllib.loads("""
+    toml = toml_loads("""
         [build-system]
         requires = ["setuptools"]
         build-backend = "setuptools.build_meta"
         """)
-    assert pyproject.PP002.check(toml)
+    assert compute_check("PP002", pyproject=toml).result
 
 
 def test_PP002_not_list():
-    toml = tomllib.loads("""
+    toml = toml_loads("""
         [build-system]
         requires = "setuptools"
         build-backend = "setuptools.build_meta"
         """)
-    assert not pyproject.PP002.check(toml)
+    assert not compute_check("PP002", pyproject=toml).result
 
 
 def test_PP002_missing():
-    toml = tomllib.loads("""
+    toml = toml_loads("""
         [project]
         name = "hi"
         version = "1.0.0"
         """)
-    assert not pyproject.PP002.check(toml)
+    assert not compute_check("PP002", pyproject=toml).result
 
 
 def test_PP003_no_wheel():
-    toml = tomllib.loads("""
+    toml = toml_loads("""
         [build-system]
         requires = ["setuptools"]
         build-backend = "setuptools.build_meta"
         """)
-    assert pyproject.PP003.check(toml)
+    assert compute_check("PP003", pyproject=toml).result
 
 
 def test_PP003_has_wheel():
-    toml = tomllib.loads("""
+    toml = toml_loads("""
         [build-system]
         requires = ["setuptools", "wheel"]
         build-backend = "setuptools.build_meta"
         """)
-    assert not pyproject.PP003.check(toml)
+    assert not compute_check("PP003", pyproject=toml).result
 
 
 def test_PP302_okay_intstr():
-    toml = tomllib.loads("""
+    toml = toml_loads("""
         [tool.pytest.ini_options]
         minversion = "7"
         """)
-    assert pyproject.PP302.check(toml)
+    assert compute_check("PP302", pyproject=toml).result
 
 
 def test_PP302_okay_verstr():
-    toml = tomllib.loads("""
+    toml = toml_loads("""
         [tool.pytest.ini_options]
         minversion = "7.0.2"
         """)
-    assert pyproject.PP302.check(toml)
+    assert compute_check("PP302", pyproject=toml).result
 
 
 def test_PP302_okay_rawint():
-    toml = tomllib.loads("""
+    toml = toml_loads("""
         [tool.pytest.ini_options]
         minversion = 7
         """)
-    assert pyproject.PP302.check(toml)
+    assert compute_check("PP302", pyproject=toml).result
 
 
 def test_PP302_okay_rawfloat():
-    toml = tomllib.loads("""
+    toml = toml_loads("""
         [tool.pytest.ini_options]
         minversion = 7.0
         """)
-    assert pyproject.PP302.check(toml)
+    assert compute_check("PP302", pyproject=toml).result
 
 
 def test_PP302_missing():
-    toml = tomllib.loads("""
+    toml = toml_loads("""
         [tool.pytest]
         ini_options = {}
         """)
-    assert not pyproject.PP302.check(toml)
+    assert not compute_check("PP302", pyproject=toml).result
 
 
 def test_PP302_too_low():
-    toml = tomllib.loads("""
+    toml = toml_loads("""
         [tool.pytest.ini_options]
         minversion = "5"
         """)
-    assert not pyproject.PP302.check(toml)
+    assert not compute_check("PP302", pyproject=toml).result
