027165105fe4c555330486252607e0bba8f36a0a
ukmo-chris.bunney <christopher.bunney@metoffice.gov.uk>
2024-12-12 09:46:53 +0000

    Stricter checks for existence of "tests" directory

--- src/sp_repo_review/checks/general.py
+++ src/sp_repo_review/checks/general.py
@@ -75,9 +75,9 @@ class PY005(General):
 
     @staticmethod
     def check(package: Traversable) -> bool:
-        "Projects must have a folder called `*test*` or `src/*/*test*`"
+        "Projects must have a folder called `tests` or `src/*/tests`"
         # Out-of-source tests
-        if len([p for p in package.iterdir() if "test" in p.name]) > 0:
+        if len([p for p in package.iterdir() if p.is_dir() and p.name == "tests"]) > 0:
             return True
 
         # In-source tests
@@ -86,7 +86,7 @@ class PY005(General):
             for pkg in src.iterdir():
                 if (
                     pkg.is_dir()
-                    and len([p for p in pkg.iterdir() if "test" in p.name]) > 0
+                    and len([p for p in pkg.iterdir() if p.is_dir() and p.name == "tests"]) > 0
                 ):
                     return True
         return False
