cba382034b5c077f5753a09b8411b7a5c5a7c183
Henry Schreiner <henry.fredrick.schreiner@cern.ch>
2025-03-13 09:38:14 -0400

    Update noxfile.py

--- {{cookiecutter.project_name}}/noxfile.py
+++ {{cookiecutter.project_name}}/noxfile.py
@@ -31,7 +31,7 @@ def pylint(session: nox.Session) -> None:
     """
     # This needs to be installed into the package environment, and is slower
     # than a pre-commit check
-    session.install("-e.", "pylint>=3.2")
+    session.install("{% if cookiecutter.backend != "mesonpy" %}-e{% endif %}.", "pylint>=3.2")
     session.run("pylint", "{{ cookiecutter.__project_slug }}", *session.posargs)
 
 
@@ -40,7 +40,7 @@ def tests(session: nox.Session) -> None:
     """
     Run the unit and regular tests.
     """
-    session.install("-e.[test]")
+    session.install("{% if cookiecutter.backend != "mesonpy" %}-e{% endif %}.[test]")
     session.run("pytest", *session.posargs)
 
 
@@ -58,7 +58,7 @@ def docs(session: nox.Session) -> None:
     args, posargs = parser.parse_known_args(session.posargs)
     serve = args.builder == "html" and session.interactive
 
-    session.install("-e.[docs]", "sphinx-autobuild")
+    session.install("{% if cookiecutter.backend != "mesonpy" %}-e{% endif %}.[docs]", "sphinx-autobuild")
 
     shared_args = (
         "-n",  # nitpicky mode
