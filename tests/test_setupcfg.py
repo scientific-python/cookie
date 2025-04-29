6c37e3a6dc3d1724becbbedf0171dbd14098b171
Henry Schreiner <henry.fredrick.schreiner@cern.ch>
2025-04-29 00:38:17 -0400

    Update test_setupcfg.py

--- tests/test_setupcfg.py
+++ tests/test_setupcfg.py
@@ -12,6 +12,7 @@ def test_scfg001():
         description = A test package
         author = Me
         author_email = me@example.com
+        """)
     assert compute_check("SCFG001", setupcfg=setupcfg).result
 
 
