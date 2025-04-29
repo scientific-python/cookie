8f3fb0a1bf241f94ccf0c0c02cbb745e28228945
Henry Schreiner <henryschreineriii@gmail.com>
2025-04-29 16:10:32 -0400

    fix: copier delay computation
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>

--- helpers/extensions.py
+++ helpers/extensions.py
@@ -3,17 +3,27 @@ import datetime
 from copier_templates_extensions import ContextHook
 
 
+class SmartDict(dict):
+    def __getitem__(self, item):
+        if item == "__ci":
+            return "github" if "github.com" in self["url"] else "gitlab"
+        if item == "__project_slug":
+            return self["project_name"].lower().replace("-", "_").replace(".", "_")
+        if item == "__type":
+            return (
+                "compiled"
+                if self["backend"] in ["pybind11", "skbuild", "mesonpy", "maturin"]
+                else "pure"
+            )
+        if item == "__answers":
+            return self["_copier_conf"]["answers_file"]
+
+        return super(item)
+
+
 class CookiecutterNamespace(ContextHook):
     def hook(self, context):
+        if context.get("_copier_phase", "") == "prompt":
+            return {}
         context["__year"] = str(datetime.datetime.today().year)
-        context["__project_slug"] = (
-            context["project_name"].lower().replace("-", "_").replace(".", "_")
-        )
-        context["__type"] = (
-            "compiled"
-            if context["backend"] in ["pybind11", "skbuild", "mesonpy", "maturin"]
-            else "pure"
-        )
-        context["__answers"] = context["_copier_conf"]["answers_file"]
-        context["__ci"] = "github" if "github.com" in context["url"] else "gitlab"
-        return {"cookiecutter": context}
+        return {"cookiecutter": SmartDict(context)}
