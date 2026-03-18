e16fb114625268c152b6d23bc2c48b28e8e97ae5
Henry Schreiner <henryschreineriii@gmail.com>
2026-03-18 14:58:04 -0400

    fix: minor touchup to template
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>

--- {{cookiecutter.project_name}}/tests/test_package.py
+++ {{cookiecutter.project_name}}/tests/test_package.py
@@ -5,5 +5,5 @@ import importlib.metadata
 import {{ cookiecutter.__project_slug }} as m
 
 
-def test_version():
+def test_version() -> None:
     assert importlib.metadata.version("{{ cookiecutter.__project_slug }}") == m.__version__
